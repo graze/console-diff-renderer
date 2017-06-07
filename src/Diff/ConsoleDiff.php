e4e76284fb33c4caf2fc5255b0abff70bbb6eed3
Harry Bragg <harry.bragg@graze.com>
2017-06-07 17:50:33 +0100

    add consolediff files

---
+++ src/Diff/ConsoleDiff.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Graze\BufferedConsole\Diff;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConsoleDiff extends FirstDiff
+{
+    const UNCLOSED_TAGS = '/<(?P<tag>[a-z;=]+)>(?!.*?<\/(?:(?P=tag)|)>)/i';
+
+    /** @var FirstDiff */
+    private $diff;
+
+    /**
+     * ConsoleDiff constructor.
+     *
+     * @param FirstDiff|null $differ
+     */
+    public function __construct($differ = null)
+    {
+        $this->diff = $differ ?: new FirstDiff();
+    }
+
+    /**
+     * @param string[] $old
+     * @param string[] $new
+     * @param int      $options
+     *
+     * @return string[]
+     */
+    public function lines(array $old, array $new, $options = OutputInterface::OUTPUT_NORMAL)
+    {
+        $diff = $this->diff->lines($old, $new);
+
+        if (($options & OutputInterface::OUTPUT_NORMAL) == OutputInterface::OUTPUT_NORMAL) {
+            // replace col number with strip_tags version to represent what is outputted to the user
+            for ($i = 0; $i < count($new); $i++) {
+                if (isset($diff[$i]) && !is_null($new[$i]) && $diff[$i]['col'] > 0) {
+                    $tags = $this->getUnclosedTags(mb_substr($new[$i], 0, $diff[$i]['col']));
+                    if (count($tags) > 0) {
+                        $diff[$i]['str'] = '<' . implode('><', $tags) . '>' . $diff[$i]['str'];
+                    }
+                    $diff[$i]['col'] = mb_strlen(strip_tags(mb_substr($new[$i], 0, $diff[$i]['col'])));
+                }
+            }
+        }
+
+        return $diff;
+    }
+
+    /**
+     * Find a list of unclosed tags
+     *
+     * @param string $string
+     *
+     * @return string[]
+     */
+    private function getUnclosedTags($string)
+    {
+        if (preg_match_all(static::UNCLOSED_TAGS, $string, $matches)) {
+            return $matches['tag'];
+        }
+        return [];
+    }
+}
---
+++ tests/unit/Diff/ConsoleDiffTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Graze\BufferedConsole\Test\Unit\Diff;
+
+use Graze\BufferedConsole\Diff\ConsoleDiff;
+use Graze\BufferedConsole\Test\TestCase;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConsoleDiffTest extends TestCase
+{
+    /**
+     * @dataProvider differenceWithTagsData
+     *
+     * @param string[] $old
+     * @param string[] $new
+     * @param int      $options
+     * @param string[] $expected
+     */
+    public function testDifferenceWithTags(array $old, array $new, $options, array $expected)
+    {
+        $diff = new ConsoleDiff();
+
+        $this->assertEquals($expected, $diff->lines($old, $new, $options));
+    }
+
+    /**
+     * @return array
+     */
+    public function differenceWithTagsData()
+    {
+        return [
+            [ // col now equals the strip_tags version (hence 6)
+                ['<info>first</info>', '<info>second</info>'],
+                ['<info>new</info>', '<info>second cake</info>'],
+                OutputInterface::OUTPUT_NORMAL,
+                [
+                    ['col' => 0, 'str' => '<info>new</info>'],
+                    ['col' => 6, 'str' => '<info> cake</info>'],
+                ],
+            ],
+            [ // multiple tags should represent all tags
+                ['<info><error>first</error></info>', '<info>second</info>'],
+                ['<info><error>new</error></info>', '<info>second cake</info>'],
+                OutputInterface::OUTPUT_NORMAL,
+                [
+                    ['col' => 0, 'str' => '<info><error>new</error></info>'],
+                    ['col' => 6, 'str' => '<info> cake</info>'],
+                ],
+            ],
+            [
+                ['<info><error>first</error></info>', '<info>second</info>'],
+                ['<info><error>new</error></info>', '<info>second cake</info>'],
+                OutputInterface::OUTPUT_RAW,
+                [
+                    ['col' => 13, 'str' => 'new</error></info>'],
+                    ['col' => 12, 'str' => ' cake</info>'],
+                ],
+            ],
+            [ // support </> as a closing tag for any tag
+                ['<info><fg=bla;bg=bar>first</>', '<info>second</info>'],
+                ['<info><fg=bla;bg=bar>first</> cake', '<info>second cake</info>'],
+                OutputInterface::OUTPUT_NORMAL,
+                [
+                    ['col' => 5, 'str' => ' cake'],
+                    ['col' => 6, 'str' => '<info> cake</info>'],
+                ],
+            ],
+        ];
+    }
+}

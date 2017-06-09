a4afb1622c9f46eadf2d265b5a1d7aedd640eb85
Harry Bragg <harry.bragg@graze.com>
2017-06-09 17:57:51 +0100

    add copyright to all files

--- src/Wrap/Wrapper.php
+++ src/Wrap/Wrapper.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of graze/buffered-console.
+ *
+ * Copyright (c) 2017 Nature Delivered Ltd. <https://www.graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license https://github.com/graze/buffered-console/blob/master/LICENSE.md
+ * @link    https://github.com/graze/buffered-console
+ */
 
 namespace Graze\BufferedConsole\Wrap;
 
